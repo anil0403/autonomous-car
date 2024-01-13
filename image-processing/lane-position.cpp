98bcd61dc1815878c6b1ebd617cae2baff032786
anil0403 <anilshrestha0403.as@gmail.com>
2024-01-14 00:43:29 +0545

    camera parameters updated

--- image-processing/lane-position.cpp
+++ image-processing/lane-position.cpp
@@ -25,13 +25,13 @@ Point2f Destination[] = {Point2f(100,0),Point2f(280,0),Point2f(100,240), Point2f
 
  void Setup ( int argc,char **argv, RaspiCam_Cv &Camera )
   {
-    Camera.set ( CAP_PROP_FRAME_WIDTH,  ( "-w",argc,argv,400 ) );
-    Camera.set ( CAP_PROP_FRAME_HEIGHT,  ( "-h",argc,argv,240 ) );
-    Camera.set ( CAP_PROP_BRIGHTNESS, ( "-br",argc,argv,50 ) );
-    Camera.set ( CAP_PROP_CONTRAST ,( "-co",argc,argv,50 ) );
-    Camera.set ( CAP_PROP_SATURATION,  ( "-sa",argc,argv,50 ) );
-    Camera.set ( CAP_PROP_GAIN,  ( "-g",argc,argv ,50 ) );
-    Camera.set ( CAP_PROP_FPS,  ( "-fps",argc,argv,100));
+    Camera.set(CAP_PROP_FRAME_WIDTH, ("-w", argc, argv, 400));
+    Camera.set(CAP_PROP_FRAME_HEIGHT, ("-h", argc, argv, 240));
+    Camera.set(CAP_PROP_BRIGHTNESS, ("-br", argc, argv, 70));
+    Camera.set(CAP_PROP_CONTRAST, ("-co", argc, argv, 60));
+    Camera.set(CAP_PROP_SATURATION, ("-sa", argc, argv, 60));
+    Camera.set(CAP_PROP_GAIN, ("-g", argc, argv, 50));
+    Camera.set(CAP_PROP_FPS, ("-fps", argc, argv, 100));
 
 }
 
